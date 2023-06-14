d0d4235625b074f5cc56ba29abc9a940a25cd073
Justin Britt <britt@uwindsor.ca>
2023-06-14 14:24:42 -0400

    Added class

---
+++ HM.HM3B.A.E.O/Classes/Comparers/LocationComparer.cs
@@ -0,0 +1,28 @@
+﻿namespace HM.HM3B.A.E.O.Classes.Comparers
+{
+    using System;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using HM.HM3B.A.E.O.Interfaces.Comparers;
+
+    internal sealed class LocationComparer : ILocationComparer
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public LocationComparer()
+        {
+        }
+
+        public int Compare(
+            Location x,
+            Location y)
+        {
+            return String.CompareOrdinal(
+                x.Id,
+                y.Id);
+        }
+    }
+}
