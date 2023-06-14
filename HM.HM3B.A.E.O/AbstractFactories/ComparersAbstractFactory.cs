18b55e19e618e8e6a432653c507d01ebe648635c
Justin Britt <britt@uwindsor.ca>
2023-06-14 13:33:55 -0400

    Added abstract factory

---
+++ HM.HM3B.A.E.O/AbstractFactories/ComparersAbstractFactory.cs
@@ -0,0 +1,37 @@
+﻿namespace HM.HM3B.A.E.O.AbstractFactories
+{
+    using System;
+
+    using log4net;
+
+    using HM.HM3B.A.E.O.Factories.Comparers;
+    using HM.HM3B.A.E.O.InterfacesAbstractFactories;
+    using HM.HM3B.A.E.O.InterfacesFactories.Comparers;
+
+    internal sealed class ComparersAbstractFactory : IComparersAbstractFactory
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public ComparersAbstractFactory()
+        {
+        }
+
+        public IOrganizationComparerFactory CreateOrganizationComparerFactory()
+        {
+            IOrganizationComparerFactory factory = null;
+
+            try
+            {
+                factory = new OrganizationComparerFactory();
+            }
+            catch (Exception exception)
+            {
+                this.Log.Error(
+                    exception.Message,
+                    exception);
+            }
+
+            return factory;
+        }
+    }
+}
