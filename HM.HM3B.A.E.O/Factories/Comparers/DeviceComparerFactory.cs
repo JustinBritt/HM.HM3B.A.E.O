396513b40bb536b0f2fc35ce487c72b000cad215
Justin Britt <britt@uwindsor.ca>
2023-06-14 13:58:19 -0400

    Added factory

---
+++ HM.HM3B.A.E.O/Factories/Comparers/DeviceComparerFactory.cs
@@ -0,0 +1,28 @@
+﻿namespace HM.HM3B.A.E.O.Factories.Comparers
+{
+    using HM.HM3B.A.E.O.Classes.Comparers;
+    using HM.HM3B.A.E.O.Interfaces.Comparers;
+    using HM.HM3B.A.E.O.InterfacesFactories.Comparers;
+
+    internal sealed class DeviceComparerFactory : IDeviceComparerFactory
+    {
+        public DeviceComparerFactory()
+        {
+        }
+
+        public IDeviceComparer Create()
+        {
+            IDeviceComparer instance = null;
+
+            try
+            {
+                instance = new DeviceComparer();
+            }
+            finally
+            {
+            }
+
+            return instance;
+        }
+    }
+}
