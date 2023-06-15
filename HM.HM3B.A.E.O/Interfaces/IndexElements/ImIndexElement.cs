ec28afd399dd3268c03785dab0f36d60d0961ee4
Justin Britt <britt@uwindsor.ca>
2023-06-15 07:48:06 -0400

    Implement IComparable

--- HM.HM3B.A.E.O/Interfaces/IndexElements/ImIndexElement.cs
+++ HM.HM3B.A.E.O/Interfaces/IndexElements/ImIndexElement.cs
@@ -1,8 +1,10 @@
 ﻿namespace HM.HM3B.A.E.O.Interfaces.IndexElements
 {
+    using System;
+
     using Hl7.Fhir.Model;
 
-    public interface ImIndexElement
+    public interface ImIndexElement : IComparable<ImIndexElement>
     {
         /// <summary>
         /// Gets the machine.
