421cc8bfaf1430a5f4bced876a4f738ae39efd1b
Justin Britt <britt@uwindsor.ca>
2023-06-16 10:54:12 -0400

    Added visitor

---
+++ HM.HM3B.A.E.O/InterfacesVisitors/Contexts/ISurgicalSpecialtyNumberAssignedTimeBlocksVisitor.cs
@@ -0,0 +1,19 @@
+﻿namespace HM.HM3B.A.E.O.InterfacesVisitors.Contexts
+{
+    using System.Collections.Generic;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+    using NGenerics.Patterns.Visitor;
+
+    using HM.HM3B.A.E.O.Interfaces.IndexElements;
+    using HM.HM3B.A.E.O.Interfaces.ParameterElements.SurgicalSpecialtyNumberAssignedTimeBlocks;
+
+    public interface ISurgicalSpecialtyNumberAssignedTimeBlocksVisitor<TKey, TValue> : IVisitor<KeyValuePair<TKey, TValue>>
+        where TKey : Organization
+        where TValue : INullableValue<int>
+    {
+        RedBlackTree<IjIndexElement, IBParameterElement> RedBlackTree { get; }
+    }
+}
---
+++ HM.HM3B.A.E.O/Visitors/Contexts/SurgicalSpecialtyNumberAssignedTimeBlocksVisitor.cs
@@ -0,0 +1,55 @@
+﻿namespace HM.HM3B.A.E.O.Visitors.Contexts
+{
+    using System.Collections.Generic;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+
+    using HM.HM3B.A.E.O.Interfaces.IndexElements;
+    using HM.HM3B.A.E.O.Interfaces.Indices;
+    using HM.HM3B.A.E.O.Interfaces.ParameterElements.SurgicalSpecialtyNumberAssignedTimeBlocks;
+    using HM.HM3B.A.E.O.InterfacesFactories.ParameterElements.SurgicalSpecialtyNumberAssignedTimeBlocks;
+    using HM.HM3B.A.E.O.InterfacesVisitors.Contexts;
+
+    internal class SurgicalSpecialtyNumberAssignedTimeBlocksVisitor<TKey, TValue> : ISurgicalSpecialtyNumberAssignedTimeBlocksVisitor<TKey, TValue>
+        where TKey : Organization
+        where TValue : INullableValue<int>
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public SurgicalSpecialtyNumberAssignedTimeBlocksVisitor(
+            IBParameterElementFactory BParameterElementFactory,
+            Ij j)
+        {
+            this.BParameterElementFactory = BParameterElementFactory;
+
+            this.j = j;
+
+            this.RedBlackTree = new RedBlackTree<IjIndexElement, IBParameterElement>();
+        }
+
+        private IBParameterElementFactory BParameterElementFactory { get; }
+
+        private Ij j { get; }
+
+        public bool HasCompleted => false;
+
+        public RedBlackTree<IjIndexElement, IBParameterElement> RedBlackTree { get; }
+
+        public void Visit(
+            KeyValuePair<TKey, TValue> obj)
+        {
+            IjIndexElement jIndexElement = this.j.GetElementAt(
+                obj.Key);
+
+            this.RedBlackTree.Add(
+                jIndexElement,
+                this.BParameterElementFactory.Create(
+                    jIndexElement,
+                    obj.Value));
+        }
+    }
+}
