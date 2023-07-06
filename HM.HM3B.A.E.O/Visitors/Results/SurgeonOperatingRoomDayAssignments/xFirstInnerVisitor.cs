c69ee6554828906e731d72f0ae7a2d6e2c69c04f
Justin Britt <britt@uwindsor.ca>
2023-07-06 14:39:38 -0400

    Added visitor

---
+++ HM.HM3B.A.E.O/Visitors/Results/SurgeonOperatingRoomDayAssignments/xFirstInnerVisitor.cs
@@ -0,0 +1,65 @@
+﻿namespace HM.HM3B.A.E.O.Visitors.Results.SurgeonOperatingRoomDayAssignments
+{
+    using System.Collections.Generic;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+    using NGenerics.Patterns.Visitor;
+
+    using HM.HM3B.A.E.O.Interfaces.Comparers;
+    using HM.HM3B.A.E.O.Interfaces.IndexElements;
+    using HM.HM3B.A.E.O.Interfaces.ResultElements.SurgeonOperatingRoomDayAssignments;
+    using HM.HM3B.A.E.O.InterfacesFactories.Dependencies.Hl7.Fhir.R4.Model;
+    using HM.HM3B.A.E.O.InterfacesVisitors.Results.SurgeonOperatingRoomDayAssignments;
+    using HM.HM3B.A.E.O.Visitors.Results.SurgeonOperatingRoomDayAssignments;
+    
+    internal sealed class xFirstInnerVisitor<TKey, TValue> : IxFirstInnerVisitor<TKey, TValue>
+        where TKey : IrIndexElement
+        where TValue : RedBlackTree<ItIndexElement, IxResultElement>
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public xFirstInnerVisitor(
+            INullableValueFactory nullableValueFactory,
+            IFhirDateTimeComparer FhirDateTimeComparer,
+            ILocationComparer locationComparer)
+        {
+            this.NullableValueFactory = nullableValueFactory;
+
+            this.FhirDateTimeComparer = FhirDateTimeComparer;
+
+            this.RedBlackTree = new RedBlackTree<Location, RedBlackTree<INullableValue<int>, INullableValue<int>>>(
+                locationComparer);
+        }
+
+        private INullableValueFactory NullableValueFactory { get; }
+
+        private IFhirDateTimeComparer FhirDateTimeComparer { get; }
+
+        public bool HasCompleted => false;
+
+        public RedBlackTree<Location, RedBlackTree<INullableValue<int>, INullableValue<int>>> RedBlackTree { get; }
+
+        public void Visit(
+            KeyValuePair<TKey, TValue> obj)
+        {
+            IrIndexElement rIndexElement = obj.Key;
+
+            RedBlackTree<ItIndexElement, IxResultElement> value = obj.Value;
+
+            IxSecondInnerVisitor<ItIndexElement, IxResultElement> innerVisitor = new xSecondInnerVisitor<ItIndexElement, IxResultElement>(
+                this.NullableValueFactory,
+                this.FhirDateTimeComparer);
+
+            value.AcceptVisitor(
+                innerVisitor);
+
+            this.RedBlackTree.Add(
+                rIndexElement.Value,
+                innerVisitor.RedBlackTree);
+        }
+    }
+}
---
+++ HM.HM3B.A.E.O/Visitors/Results/SurgeonOperatingRoomDayAssignments/xOuterVisitor.cs
@@ -0,0 +1,73 @@
+﻿namespace HM.HM3B.A.E.O.Visitors.Results.SurgeonOperatingRoomDayAssignments
+{
+    using System.Collections.Generic;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+    using NGenerics.Patterns.Visitor;
+
+    using HM.HM3B.A.E.O.Interfaces.Comparers;
+    using HM.HM3B.A.E.O.Interfaces.IndexElements;
+    using HM.HM3B.A.E.O.Interfaces.ResultElements.SurgeonOperatingRoomDayAssignments;
+    using HM.HM3B.A.E.O.InterfacesFactories.Dependencies.Hl7.Fhir.R4.Model;
+    using HM.HM3B.A.E.O.InterfacesVisitors.Results.SurgeonOperatingRoomDayAssignments;
+    using HM.HM3B.A.E.O.Visitors.Results.SurgeonOperatingRoomDayAssignments;
+
+    internal sealed class xOuterVisitor<TKey, TValue> : IxOuterVisitor<TKey, TValue>
+        where TKey : IsIndexElement
+        where TValue : RedBlackTree<IrIndexElement, RedBlackTree<ItIndexElement, IxResultElement>>
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public xOuterVisitor(
+            INullableValueFactory nullableValueFactory,
+            IFhirDateTimeComparer FhirDateTimeComparer,
+            ILocationComparer locationComparer,
+            IOrganizationComparer organizationComparer)
+        {
+            this.NullableValueFactory = nullableValueFactory;
+
+            this.FhirDateTimeComparer = FhirDateTimeComparer;
+
+            this.LocationComparer = locationComparer;
+
+            this.RedBlackTree = new RedBlackTree<Organization, RedBlackTree<Location, RedBlackTree<INullableValue<int>, INullableValue<int>>>>(
+                organizationComparer);
+        }
+
+        private INullableValueFactory NullableValueFactory { get; }
+
+        private IFhirDateTimeComparer FhirDateTimeComparer { get; }
+
+        private INullableValueintComparer NullableValueintComparer { get; }
+
+        private ILocationComparer LocationComparer { get; }
+
+        public bool HasCompleted => false;
+
+        public RedBlackTree<Organization, RedBlackTree<Location, RedBlackTree<INullableValue<int>, INullableValue<int>>>> RedBlackTree { get; }
+
+        public void Visit(
+            KeyValuePair<TKey, TValue> obj)
+        {
+            IsIndexElement sIndexElement = obj.Key;
+
+            RedBlackTree<IrIndexElement, RedBlackTree<ItIndexElement, IxResultElement>> value = obj.Value;
+
+            IxFirstInnerVisitor<IrIndexElement, RedBlackTree<ItIndexElement, IxResultElement>> innerVisitor = new xFirstInnerVisitor<IrIndexElement, RedBlackTree<ItIndexElement, IxResultElement>>(
+                this.NullableValueFactory,
+                this.FhirDateTimeComparer,
+                this.LocationComparer);
+
+            value.AcceptVisitor(
+                innerVisitor);
+
+            this.RedBlackTree.Add(
+                sIndexElement.Value,
+                innerVisitor.RedBlackTree);
+        }
+    }
+}
